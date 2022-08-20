a6966dcc4e59922ac766c3a280929a02f888af35
Dennis Kamau <devjam.ke@gmail.com>
2022-08-20 13:25:40 +0300

    Create Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/landing_page'
+  get 'static_pages/pricing'
+  get 'static_pages/privacy'
+  get 'static_pages/terms'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
