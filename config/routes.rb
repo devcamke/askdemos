6e41fe54e5d574485fbf8237b1e4ea42b7f9e679
Dennis Kamau <devjam.ke@gmail.com>
2022-08-20 13:31:52 +0300

    Add routes for the other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root 'static_pages#landing_page'
-  get 'static_pages/pricing'
-  get 'static_pages/privacy'
-  get 'static_pages/terms'
+  get 'pricing', to: 'static_pages#pricing'
+  get 'privacy', to: 'static_pages#privacy'
+  get 'terms', to: 'static_pages#terms'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
